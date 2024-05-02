0a23fe5d6af97f6d6721d1eaffa0d9cfa1fdf28c
Horbal Daniil <daniil.horbal@lnu.edu.ua>
2024-05-02 13:10:51 +0300

    Added signup page design
    - Signup and Login controllers moved to the AuthorizationController file

---
+++ StudyNow.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyNow.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [Route("")]
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+    }
+}
