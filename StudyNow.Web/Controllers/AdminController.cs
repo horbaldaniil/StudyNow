fb0171392aaa5fdca604c7fe7492fe2889dc1b54
Horbal Daniil <daniil.horbal@lnu.edu.ua>
2024-05-19 01:02:45 +0300

    Added design for admin section

---
+++ StudyNow.Web/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyNow.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Subjects()
+        {
+            return View();
+        }
+
+        public IActionResult Groups()
+        {
+            return View();
+        }
+
+        public IActionResult Lessons()
+        {
+            return View();
+        }
+
+        public IActionResult Assignments()
+        {
+            return View();
+        }
+    }
+}
